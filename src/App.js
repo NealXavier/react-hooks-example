846ae8bb5634a4362c58163defde931da1960d02
wxliujiachun <wxliujiachun@gfpartner.com.cn>
2021-03-27 12:16:57 +0800

    multiple context-consumer

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import React, { createContext } from 'react';
 import './App.css';
 
 const BatteryContext = createContext()
+const OnlineContext = createContext()
 
-const Leaf = () => (<BatteryContext.Consumer>{battery=><h1>battery:{battery}</h1>}</BatteryContext.Consumer>)
+const Leaf = () => (<BatteryContext.Consumer>{battery=><OnlineContext.Consumer>{online=><h1>battery:{battery},online:{+online}</h1>}</OnlineContext.Consumer>}</BatteryContext.Consumer>)
 const Middle = () => <Leaf />
 
 class App extends React.Component {
   state = {
-    battery : 60
+    battery : 60,
+    online : true
   }
   render(){
-    const {battery} = this.state
+    const {battery,online} = this.state
     return (
       <BatteryContext.Provider value={battery}>
-        <button 
-          type="button"
-          onClick={()=>this.setState({battery:battery-1})}>
-            countDown
-        </button>
-        <Middle />
+        <OnlineContext.Provider value={online}>
+          <button 
+            type="button"
+            onClick={()=>this.setState({battery:battery-1})}>
+              countDown
+          </button>
+          <button 
+            type="button"
+            onClick={()=>this.setState({online:!online})}>
+              switch
+          </button>
+          <Middle />
+        </OnlineContext.Provider>
       </BatteryContext.Provider>
     )
   }
