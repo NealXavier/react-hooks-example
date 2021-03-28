d5bf72ba2ea528dab68a97f2d025b2144f863301
wxliujiachun <wxliujiachun@gfpartner.com.cn>
2021-03-28 20:33:26 +0800

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React, { useState } from 'react';
+import React,{useEffect, useState} from 'react';
 
-// let id = 1
+
 const App = (props) => {
-  // let name,setName;
-  // let count,setCount;
-
-  // id+=1
-  // React Hook "useState" is called conditionally. React Hooks must be called 
-  // in the exact same order in every component render  react-hooks/rules-of-hooks
-  // odd times
-  // if(id & 1){
-  //   [count,setCount] = useState(0);
-  //   [name,setName] = useState(`mike`)
-  // }else{
-  //   [name,setName] = useState('mike')
-  //   [count,setCount] = useState(0)
-  // }
-
-
-  // when `defaultCount` runs much time, and runs when render every time, now provide a function in `useState`
-  // will runs once when initial first time.
-  // const defaultCount = props.defaultCount || 0
-  const [count,setCount] = useState(()=>{
-    console.log('defaultCount:')
-    return props.defaultCount || 0
+  const [size,setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight
   })
+  const [count,setCount] = useState(0)
+
+  const resizeScreen = () =>{
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight
+    })
+  } 
+
+  // useEffect: runs once when initial to addEventListerner & removeEventListerner
+  useEffect(()=>{
+    window.addEventListener('resize',resizeScreen,false)
+    return ()=>{window.removeEventListener('resize',resizeScreen,false)}
+  },[])
+
+  // useEffect: runs when count changed
+  useEffect(()=>{
+    document.title = count
+  },[count])
 
   return (
-      <> 
-        count: {count} <br/>
-        <button type="button" onClick={()=>{setCount(count+1)}}>onPress</button>
+      <>
+        <button type='button' onClick={()=>setCount(count+1)}>click</button><br/>
+        Click:{ count } <br/>
+        size:{size.width} * {size.height}
       </>
   )
 }
