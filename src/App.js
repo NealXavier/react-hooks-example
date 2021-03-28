68d9a25ed6f115e957ff94932fa9d1e9c4219776
wxliujiachun <wxliujiachun@gfpartner.com.cn>
2021-03-28 20:56:44 +0800

    usecontext

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import React,{useEffect, useState} from 'react';
+import React,{createContext, useContext, useState} from 'react';
 
+const CounterContext = createContext()
+
+const Counter = () => {
+  const count = useContext(CounterContext)
+  return (
+    <h1>{count}</h1>
+  )
+}
 
 const App = (props) => {
-  const [size,setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight
-  })
-  const [count,setCount] = useState(0)
-
-  const resizeScreen = () =>{
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight
-    })
-  } 
-
-  // useEffect: runs once when initial to addEventListerner & removeEventListerner
-  useEffect(()=>{
-    window.addEventListener('resize',resizeScreen,false)
-    return ()=>{window.removeEventListener('resize',resizeScreen,false)}
-  },[])
-
-  // useEffect: runs when count changed
-  useEffect(()=>{
-    document.title = count
-  },[count])
-
+  const count = useState(1)
   return (
       <>
-        <button type='button' onClick={()=>setCount(count+1)}>click</button><br/>
-        Click:{ count } <br/>
-        size:{size.width} * {size.height}
+        <CounterContext.Provider value={count}>
+            <Counter />
+        </CounterContext.Provider>
       </>
   )
 }
